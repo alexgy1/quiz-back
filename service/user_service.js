9a0e7a513502633efd5bf49bb6ba1dfb6036d6c6
alex <gaunyu.wu@orange.com>
2022-01-10 14:46:29 +0800

    add admin client id in user service

--- service/user_service.js
+++ service/user_service.js
@@ -1,34 +1,37 @@
-'use strict'
+"use strict";
 
-const bent = require('bent');
-const config = require('config');
+const bent = require("bent");
+const config = require("config");
 
-
-const ORANGE_AUTH_TOKEN_VERIFICATION_URL = config.get('auth.url');
-const CLIENT_ID = config.get('auth.clientid');
-const API_KEY_ADMIN = config.get('auth.adminkey');
-const API_KEY = config.get('auth.key');
-
+const ORANGE_AUTH_TOKEN_VERIFICATION_URL = config.get("auth.url");
+const CLIENT_ID = config.get("auth.clientid");
+const CLIEND_ID_ADMIN = config.get("auth.adminclientid");
+const API_KEY_ADMIN = config.get("auth.adminkey");
+const API_KEY = config.get("auth.key");
 
 module.exports = {
-  verifyToken: async function(token, isAdmin) {
-    const getJSON = bent('json')
+  verifyToken: async function (token, isAdmin) {
+    const getJSON = bent("json");
     try {
-      console.log(ORANGE_AUTH_TOKEN_VERIFICATION_URL)
-      const user = await getJSON(ORANGE_AUTH_TOKEN_VERIFICATION_URL + token, 
-        "GET", {
-          "client-id": CLIENT_ID,
-          "api-key": isAdmin? API_KEY_ADMIN: API_KEY }
-      )
+      console.log(ORANGE_AUTH_TOKEN_VERIFICATION_URL);
+      const user = await getJSON(
+        ORANGE_AUTH_TOKEN_VERIFICATION_URL + token,
+        "GET",
+        {
+          "client-id": isAdmin ? CLIEND_ID_ADMIN : CLIENT_ID,
+          "api-key": isAdmin ? API_KEY_ADMIN : API_KEY,
+        }
+      );
 
-      return {id: user.sub, email: user.email};
+      return { id: user.sub, email: user.email };
     } catch (e) {
       const error = await e.json();
-      if(error.code != 403) {
-        console.error("FATAL verification interface error, please check the code")
+      if (error.code != 403) {
+        console.error(
+          "FATAL verification interface error, please check the code"
+        );
       }
-      return false
+      return false;
     }
-  }
-  
-}
+  },
+};
